21fd515537a280c5bcf51bf79747080154955bac
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-28 15:17:08 -0700

    Adding basic css files

--- my-site.rb
+++ my-site.rb
@@ -2,20 +2,20 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
-  get "/" do
-  	erb :index
-  end
+	get "/" do
+		erb :index
+	end
 
-  get "/taco_corn" do
-  	erb :taco_corn
-  end
+	get "/taco_corn" do
+		erb :taco_corn
+	end
 
-  get "/blog" do
-  	erb :blog
-  end
+	get "/blog" do
+		erb :blog
+	end
 
-  get "/index" do
-  	erb :index
-  end
+	get "/index" do
+		erb :index
+	end
 
 end
