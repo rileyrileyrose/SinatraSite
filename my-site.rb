1add72651d6791425ee037ba5a34e5551bde6fa0
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-29 14:49:59 -0700

    Added a layout page, began setting it up

--- my-site.rb
+++ my-site.rb
@@ -3,19 +3,23 @@ require "sinatra"
 class MySite < Sinatra::Base
 
 	get "/" do
+		@title = "One Taco to Rule Them All"
+		erb :index
+	end
+
+	get "/index" do
+		@title = "One Taco to Rule Them All"
 		erb :index
 	end
 
 	get "/taco_corn" do
+		@title = "TacoCorns Unite!"
 		erb :taco_corn
 	end
 
 	get "/blog" do
+		@title = "My Ideas are Important."
 		erb :blog
-	end
-
-	get "/index" do
-		erb :index
 	end
 
 end
