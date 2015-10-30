f904e3d16cf1f303766e9cda7ec51b4bde1dc300
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-30 10:44:30 -0700

    You can name your own TacoCorn

--- my-site.rb
+++ my-site.rb
@@ -4,16 +4,17 @@ class Taco
 end
 
 class Unicorn
-	attr_reader :name, :horn_size, :hunger
+	attr_reader :horn_size, :hunger
+	attr_accessor :name
 
 	def initialize
-		@name = give_name
+		@name = ""
 		@horn_size = rand(2..10)
 		@hunger = @horn_size
 	end
 
 	def introduce
-		@intro = "Meet #{name}, the TacoCorn! \n#{name} loves to eat tacos."
+		@intro = "Meet #{name}, the TacoCorn. \n#{name} loves to eat tacos."
 	end
 
 	def self.introduce_new
@@ -22,14 +23,14 @@ class Unicorn
 	end
 
 	def give_name
-		names = ['Charlie', 'Pericles', 'Desmond', 'Scarlett', 'Janet', 'Gertrude']
+		names = ['Charlie', 'Pericles', 'Desmond', 'Scarlett', 'Janet', 'Gertrude', 'Apple', 'Onion', 'WaterfallRiverSparkle', 'Unitarded', 'Gregory']
 		length = names.length - 1
 		name = names[rand(0..length)]
 	end
 end
 
 def random_tacocorn
-	@taco_corns = ["hipster_tacocorn.png", "taco_unicorn.jpeg", "hipster_tacocorn1.jpeg", "tacocorn1.jpeg", "brony.jpeg", "tacocorn2.jpeg", "tacocorn3.jpeg"]
+	@taco_corns = ["hipster_tacocorn.png", "taco_unicorn.jpeg", "hipster_tacocorn2.jpeg", "tacocorn1.jpeg", "brony.jpeg", "tacocorn2.jpeg", "tacocorn3.jpeg"]
 	length = @taco_corns.length - 1
 	return @taco_corns[rand(0..length)]
 end
@@ -59,9 +60,20 @@ class MySite < Sinatra::Base
 
 	get "/tacocorn_creator" do
 		@title = "TacoCorn Creator"
-		@uni = Unicorn.introduce_new
+		@uni = Unicorn.new
+		@uni.name = @uni.give_name
+		@intro = @uni.introduce
 		@tacocorn = random_tacocorn
 		erb :tacocorn_creator
+	end
+
+	post '/named_tacocorn' do
+		@uni = Unicorn.new
+	  @name =  params[:name]
+	  @uni.name = @name
+	  @intro = @uni.introduce
+	  @tacocorn = random_tacocorn
+	  erb :named_tacocorn
 	end
 end
 
