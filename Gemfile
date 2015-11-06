df0f09f098b80a4ac2d140bc4c31cc20c4de6d4d
Riley Spicer <riley.r.spicer@gmail.com>
2015-11-06 10:39:24 -0800

    Removed sqlite3:

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ source "https://rubygems.org"
 ruby '2.2.3'
 
 gem 'sinatra'
-gem 'sqlite3'
 
 group :development do
 	gem 'pry'
--- lib/database.rb
+++ lib/database.rb
@@ -1,29 +1,29 @@
-require "sqlite3"
+# require "sqlite3"
 
-module TacoCorn
-  class Database
+# module TacoCorn
+#   class Database
 
-    def initialize(db_name)
-      @db = SQLite3::Database.new(db_name)
-    end
+#     def initialize(db_name)
+#       @db = SQLite3::Database.new(db_name)
+#     end
 
-    def create_schema
-      @db.execute('
-        CREATE TABLE tacocorns (
-          id INTEGER PRIMARY KEY,
-          name TEXT NOT NULL,
-          hunger INTEGER NOT NULL,
-          horn_size INTEGER NOT NULL
-          description TEXT NULL, 
-        );')
-    end
+#     def create_schema
+#       @db.execute('
+#         CREATE TABLE tacocorns (
+#           id INTEGER PRIMARY KEY,
+#           name TEXT NOT NULL,
+#           hunger INTEGER NOT NULL,
+#           horn_size INTEGER NOT NULL
+#           description TEXT NULL, 
+#         );')
+#     end
 
-    def create_tacocorn(name, horn_size, hunger, description)
-			@db.execute('
-				INSERT INTO tacocorns (name, horn_size, hunger, description)
-				VALUES(?, ?, ?, ?)
-				', name, horn_size, hunger, description)
-		end
-  end
+#     def create_tacocorn(name, horn_size, hunger, description)
+# 			@db.execute('
+# 				INSERT INTO tacocorns (name, horn_size, hunger, description)
+# 				VALUES(?, ?, ?, ?)
+# 				', name, horn_size, hunger, description)
+# 		end
+#   end
 
-end
+# end
